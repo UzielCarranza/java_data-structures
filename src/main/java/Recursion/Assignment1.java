6691848f081b743ffb7fedd76bb7fc056f899c56
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-07-07 11:42:00 -0500

    initial project

---
+++ src/main/java/Recursion/Assignment1.java
@@ -0,0 +1,25 @@
+package Recursion;
+
+
+public class Assignment1 {
+
+    public static void main(String[] args) {
+//        calling an instance of Recursion.Fibonacci to access it's methods
+        Fibonacci fibonacci = new Fibonacci();
+
+        //*********************************************************
+        //****Assignment 1, Section 1
+        //*********************************************************
+        System.out.printf("\n");
+        System.out.printf("**********Section 1 **********\n");
+        System.out.printf("\n");
+        System.out.printf("The Recursion.Fibonacci value of 10 is: ");
+        System.out.printf("%d", fibonacci.fibonacci(10));
+        System.out.println("\n");
+
+
+
+    }
+
+
+}
---
+++ src/main/java/Recursion/Fibonacci.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class Fibonacci {
+
+    public Fibonacci() {
+    }
+
+    public int fibonacci(int number)
+    {
+        if(number == 1 || number == 2)
+        {
+            return 1;
+        }
+        return fibonacci(number-1) + fibonacci(number -2);
+    }
+}
