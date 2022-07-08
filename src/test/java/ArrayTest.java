67f3925d7181347e48d91c88f530374ced3ca825
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-07-07 22:36:40 -0500

    added test units for arrays examples

--- src/test/java/ArrayTest.java
+++ src/test/java/ArrayTest.java
@@ -1,7 +1,15 @@
+import DataStructures.Array;
 import org.testng.annotations.Test;
 
-public class Array {
+public class ArrayTest {
 
     @Test
+    public void ArrayTestToUpperCase(){
+
+        Array arrayExercise = new Array();
+
+        String[] input = new String[]{"mon", "tues", "wed"};
+        arrayExercise.arrayToUpperCase(input);
+    }
 
 }
