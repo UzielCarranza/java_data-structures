ac633adc4c169b3794ae0f7a89c17c14fff89dda
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-07-07 22:44:37 -0500

    added example to increment array's length, added testing methods

--- src/main/java/DataStructures/Array.java
+++ src/main/java/DataStructures/Array.java
@@ -25,10 +25,18 @@ public class Array {
 
     }
 
-
+// methods with tests
     public void arrayToUpperCase(String[] input){
         for (int i = 0; i < input.length; i++){
             input[i] = input[i].toUpperCase();
         }
     }
+
+
+    public void incrementArray(int[] input){
+        for (int i = 0; i < input.length; i++){
+            input[i] = input[i] + 1;
+        }
+    }
+
 }
--- src/test/java/ArrayTest.java
+++ src/test/java/ArrayTest.java
@@ -1,15 +1,26 @@
 import DataStructures.Array;
 import org.testng.annotations.Test;
 
+import static org.testng.internal.junit.ArrayAsserts.assertArrayEquals;
+
+
 public class ArrayTest {
+
+    Array arrayExercise = new Array();
 
     @Test
     public void ArrayTestToUpperCase(){
 
-        Array arrayExercise = new Array();
 
         String[] input = new String[]{"mon", "tues", "wed"};
         arrayExercise.arrayToUpperCase(input);
+    }
+
+    @Test
+    public void TestIncrementArray(){
+        int[] input = new int[]{1,2,3,4,5};
+        arrayExercise.incrementArray(input);
+        assertArrayEquals(new int[]{2,3,4,5,6}, input);
     }
 
 }
