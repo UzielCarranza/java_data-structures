ce540c4ea14770e4b12026666e05b7d43ae58d6b
UzielCarranza <uzielcarranzac97@gmail.com>
2022-07-28 16:29:28 -0400

    implemented a quick sort algorithm, using recursion, that will sort the grade array from highest to lowest

--- src/main/java/DataStructures2/MyProgramPartB.java
+++ src/main/java/DataStructures2/MyProgramPartB.java
@@ -13,8 +13,9 @@ public class MyProgramPartB {
     public void bubbleSort() {
         System.out.println("********** Section 1 – Bubble Sort **********" + "\n");
 
-//        create array that holds student grades
+        //        create array that holds student grades
         int[] studentGrades = {65, 95, 75, 55, 56, 90, 98, 88, 97, 78};
+
         printArray(studentGrades);
         System.out.println("\n");
         sortArrayDescBS(studentGrades);
@@ -59,12 +60,46 @@ public class MyProgramPartB {
         }
     }
 
-//    print all elements within array of integers
-    public void printArray(int[] grades){
+    //    print all elements within array of integers
+    public void printArray(int[] grades) {
         System.out.println("The unsorted list of grades is,");
-        for (int grade : grades){
+        for (int grade : grades) {
             System.out.println(grade);
         }
+    }
+
+
+    public void quickSort() {
+        //        create array that holds student grades
+        int[] studentGrades = {65, 95, 75, 55, 56, 90, 98, 88, 97, 78};
+        System.out.println("*********** Section: 2 – Quick Sort ***********" + "\n");
+        sortArrayDescQS(studentGrades, studentGrades.length);
+        for (int grade : studentGrades) {
+            System.out.println(grade);
+        }
+
+    }
+
+    //    implements a quick sort algorithm, using recursion, that will sort the grade array from highest to lowest
+    public void sortArrayDescQS(int[] grades, int num) {
+//        based case
+        if (num == 1) {
+            return;
+        }
+
+        // One pass of bubble sort. After
+        // this pass, the largest element
+        // is moved (or bubbled) to end.
+        for (int i = 0; i < num - 1; i++)
+            if (grades[i] > grades[i + 1]) {
+                // swap arr[i], arr[i+1]
+                int temp = grades[i];
+                grades[i] = grades[i + 1];
+                grades[i + 1] = temp;
+            }
+        // Largest element is fixed,
+        // recur for remaining array
+        sortArrayDescQS(grades, num - 1);
     }
 
 }
