61c1cc9de0ce84a7e01560a2ba9df126136b2877
UzielCarranza <uzielcarranzac97@gmail.com>
2022-07-27 21:46:08 -0400

    added method that sorts an array from highest to lowest

--- src/main/java/DataStructures2/MyProgramPartB.java
+++ src/main/java/DataStructures2/MyProgramPartB.java
@@ -1,5 +1,8 @@
 package DataStructures2;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class MyProgramPartB {
 //    constructor
     public MyProgramPartB(){
@@ -12,10 +15,27 @@ public class MyProgramPartB {
 
 //        create array that holds student grades
         int[] studentGrades = {65, 95, 75, 55, 56, 90, 98, 88, 97,78};
+        sortArrayDescBS(studentGrades);
     }
 
 //     implements a bubble sort algorithm that will sort the grade array from highest to lowest
-    public void sortArrayDescBS(){
+    public void sortArrayDescBS(int[] grades){
+        int temp;
+        for (int i = 0; i < grades.length; i++){
+            for (int j = 0; j < grades.length; j++){
+                if (grades[i] > grades[j]){
+                    temp = grades[i];
+                    grades[i] = grades[j];
+                    grades[j] = temp;
+                }
+            }
+        }
+        System.out.println("Array elements in descending order");
+//        }
+        for (int num : grades){
+            System.out.println(num);
+        }
+
 
     }
 }
