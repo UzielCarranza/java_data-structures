d27f851ac443f8cd53a9c9a9e2d078231192dc7e
UzielCarranza <uzielcarranzac97@gmail.com>
2022-07-27 21:19:54 -0400

    created a method that implements a bubble sort algorith that will sort the grade aarray from highest to lowest

---
+++ src/main/java/DataStructures2/MyProgramPartB.java
@@ -0,0 +1,21 @@
+package DataStructures2;
+
+public class MyProgramPartB {
+//    constructor
+    public MyProgramPartB(){
+
+    }
+
+//    methods
+    public void bubbleSort(){
+        System.out.println("********** Section 1 – Bubble Sort **********" + "\n");
+
+//        create array that holds student grades
+        int[] studentGrades = {65, 95, 75, 55, 56, 90, 98, 88, 97,78};
+    }
+
+//     implements a bubble sort algorithm that will sort the grade array from highest to lowest
+    public void sortArrayDescBS(){
+
+    }
+}
