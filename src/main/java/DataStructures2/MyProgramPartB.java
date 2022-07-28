efccb7e843afd259a51c8bde3f39dcbeacf70a0a
UzielCarranza <uzielcarranzac97@gmail.com>
2022-07-27 21:52:14 -0400

    created method that sort an array of numbers from ascendin order

--- src/main/java/DataStructures2/MyProgramPartB.java
+++ src/main/java/DataStructures2/MyProgramPartB.java
@@ -4,26 +4,28 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class MyProgramPartB {
-//    constructor
-    public MyProgramPartB(){
+    //    constructor
+    public MyProgramPartB() {
 
     }
 
-//    methods
-    public void bubbleSort(){
+    //    methods
+    public void bubbleSort() {
         System.out.println("********** Section 1 – Bubble Sort **********" + "\n");
 
 //        create array that holds student grades
-        int[] studentGrades = {65, 95, 75, 55, 56, 90, 98, 88, 97,78};
+        int[] studentGrades = {65, 95, 75, 55, 56, 90, 98, 88, 97, 78};
         sortArrayDescBS(studentGrades);
+        System.out.println("\n");
+        sortArrayAscBS(studentGrades);
     }
 
-//     implements a bubble sort algorithm that will sort the grade array from highest to lowest
-    public void sortArrayDescBS(int[] grades){
+    //     implements a bubble sort algorithm that will sort the grade array from highest to lowest
+    public void sortArrayDescBS(int[] grades) {
         int temp;
-        for (int i = 0; i < grades.length; i++){
-            for (int j = 0; j < grades.length; j++){
-                if (grades[i] > grades[j]){
+        for (int i = 0; i < grades.length; i++) {
+            for (int j = 0; j < grades.length; j++) {
+                if (grades[i] > grades[j]) {
                     temp = grades[i];
                     grades[i] = grades[j];
                     grades[j] = temp;
@@ -31,11 +33,25 @@ public class MyProgramPartB {
             }
         }
         System.out.println("Array elements in descending order");
-//        }
-        for (int num : grades){
+        for (int num : grades) {
             System.out.println(num);
         }
+    }
 
-
+    public void sortArrayAscBS(int[] grades) {
+        int temp;
+        for (int i = 0; i < grades.length; i++) {
+            for (int j = 0; j < grades.length; j++) {
+                if (grades[i] < grades[j]) {
+                    temp = grades[i];
+                    grades[i] = grades[j];
+                    grades[j] = temp;
+                }
+            }
+        }
+        System.out.println("Array elements in ascending order");
+        for (int num : grades) {
+            System.out.println(num);
+        }
     }
 }
