6c1d1b867ed41187f43da1d323575ffd7072fbf5
UzielCarranza <uzielcarranzac97@gmail.com>
2022-08-19 23:47:41 -0400

    using Document builder factory to access the xml file... using document builder to parse the xml file and get the name of the root element

--- src/main/java/StudentGrades.java
+++ src/main/java/StudentGrades.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class StudentGrades {
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
+public class StudentGrades {
+
+    public static void main(String[] args) {
+
+        try {
+            File xmlFile = new File("/Users/uzielcarranzacharro/IdeaProjects/java_projects/src/main/java/studentGrdes.xml");
+
+//        xml parsers
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document document = documentBuilder.parse(xmlFile);
+
+            document.getDocumentElement().normalize();
+//            this gets the wrapping element
+            System.out.println("Root Element :" + document.getDocumentElement().getNodeName());
+
+//            get all elements within the Root element
+            NodeList nodeList = document.getElementsByTagName("grade");
+//            print the number of elements
+            System.out.println(nodeList.getLength());
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
 }
