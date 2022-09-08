e2405ab56b3326aef32fc2f985062f0bb24d1c14
UzielCarranza <uzielcarranzac97@gmail.com>
2022-09-08 14:42:01 -0400

    added optimized solution for findMaximum algorithm (added detail documentation as comments within the code

--- src/main/java/Algorithms/findMaximum.java
+++ src/main/java/Algorithms/findMaximum.java
@@ -1,19 +1,37 @@
 package Algorithms;
 
 public class findMaximum {
-    public static int findMaximum(int a, int b, int c){
-        if (a > b){
-            if (a > c || a == c){
+    public static int findMaximum(int a, int b, int c) {
+        if (a > b) {
+            if (a > c || a == c) {
                 return a;
             }
         }
-        if (b > c){
+        if (b > c) {
             return b;
         }
         return c;
     }
 
+
+    //    new implementation only does 2 comparisons per call
+    public static int findMaximumOptimize(int a, int b, int c) {
+        int max = a;
+
+//        at this point, max contains the maximum of A and B
+        if (b > max) {
+            max = b;
+        }
+//        assign maximum value to if c is greather than A and B
+        if (c > max) {
+            max = c;
+        }
+
+        return max;
+    }
+
     public static void main(String[] args) {
-        System.out.println(findMaximum(1,22,3));
+        System.out.println(findMaximum(1, 22, 3));
+        System.out.println(findMaximumOptimize(1,22,3));
     }
 }
