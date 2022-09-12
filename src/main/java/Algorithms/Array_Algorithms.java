5120fba150b5b40e1f335ce602999587c7ca1b80
UzielCarranza <uzielcarranzac97@gmail.com>
2022-09-12 13:03:26 -0400

    added an optimized algorithm to the linear search method by using stream api to search for an item within an array of integers. This helps by helping us prevent null exceptions in a single line of code

--- src/main/java/Algorithms/Array_Algorithms.java
+++ src/main/java/Algorithms/Array_Algorithms.java
@@ -1,21 +1,38 @@
 package Algorithms;
 
+import DataStructures.Array;
+
+import java.util.Arrays;
+import java.util.OptionalInt;
+
 public class Array_Algorithms {
 
-//    searching for data within an array data structure
-    public static Integer linearSearch(int[] arr, int item){
-        for (int current : arr){
-            if (current == item){
+    //    searching for data within an array data structure
+    public static Integer linearSearch(int[] arr, int item) {
+        for (int current : arr) {
+            if (current == item) {
                 return item;
             }
         }
         return null;
     }
 
+
+    //    searching for data within an array data structure using array stream
+//    an optional acts as a container object, to help prevent null exceptions
+    public static OptionalInt linearSearch2(int[] arr, int item) {
+        return Arrays.stream(arr).filter(x -> x == item).findFirst();
+    }
+
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,};
+        int[] arr = {1, 2, 3, 4, 5,};
         System.out.println(linearSearch(arr, 1));
         System.out.println(linearSearch(arr, 9));
+        System.out.println();
+
+        System.out.println("LINEAR SEARCH USING STREAM API");
+        linearSearch2(arr, 1).ifPresent(System.out::println);
+        linearSearch2(arr, 9).ifPresent(System.out::println);
 
     }
 }
