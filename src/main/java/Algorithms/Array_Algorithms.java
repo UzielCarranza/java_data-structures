7a107db687d40262929f4aea7419148432122f3a
UzielCarranza <uzielcarranzac97@gmail.com>
2022-09-12 13:17:20 -0400

    added algorithm that does binary search to search for an item within an array of items

--- src/main/java/Algorithms/Array_Algorithms.java
+++ src/main/java/Algorithms/Array_Algorithms.java
@@ -24,6 +24,38 @@ public class Array_Algorithms {
         return Arrays.stream(arr).filter(x -> x == item).findFirst();
     }
 
+
+    //    BINARY SEARCH
+    public static boolean binarySearch(int[] arr, int item) {
+//        declare min and max values
+        int min = 0;
+        int max = arr.length - 1;
+
+        while (min <= max) {
+//            continue to search for the item
+//            declare mid value
+            int mid = (min + max) / 2;
+//            check if item is at the midpoint of the array
+            if (item == arr[mid]) {
+//                found the item
+                return true;
+            }
+//            otherwise, search through a subarray of this inner array
+            else if (item < arr[mid]) {
+//                search through the front half of the array
+                max = mid - 1;
+            } else {
+//                if the item we are searching for is greater than the item at the midpoint
+//                then we need to search through the back half of the inner array
+                min = mid + 1;
+            }
+        }
+
+//        if item was not found
+        return false;
+
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5,};
         System.out.println(linearSearch(arr, 1));
@@ -33,6 +65,10 @@ public class Array_Algorithms {
         System.out.println("LINEAR SEARCH USING STREAM API");
         linearSearch2(arr, 1).ifPresent(System.out::println);
         linearSearch2(arr, 9).ifPresent(System.out::println);
+        System.out.println();
+        System.out.println("BINARY SEARCH");
+        System.out.println(binarySearch(arr, 1));
+        System.out.println(binarySearch(arr, 9));
 
     }
 }
