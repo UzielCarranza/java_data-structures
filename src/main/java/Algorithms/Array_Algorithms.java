581e575c811f71d1bf164e969cabeb1238a2bb03
UzielCarranza <uzielcarranzac97@gmail.com>
2022-09-12 12:50:04 -0400

    Added algorithm that performs linear search

---
+++ src/main/java/Algorithms/Array_Algorithms.java
@@ -0,0 +1,21 @@
+package Algorithms;
+
+public class Array_Algorithms {
+
+//    searching for data within an array data structure
+    public static Integer linearSearch(int[] arr, int item){
+        for (int current : arr){
+            if (current == item){
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,};
+        System.out.println(linearSearch(arr, 1));
+        System.out.println(linearSearch(arr, 9));
+
+    }
+}
