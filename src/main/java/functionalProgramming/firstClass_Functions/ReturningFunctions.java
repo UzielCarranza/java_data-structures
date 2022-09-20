d5254ab6128a9fb2b38a5dde93d80c45ccbb18aa
UzielCarranza <uzielcarranzac97@gmail.com>
2022-09-20 15:14:22 -0400

    created function that takes a function that decides how many times we are going to multiple a number for

--- src/main/java/functionalProgramming/firstClass_Functions/ReturningFunctions.java
+++ src/main/java/functionalProgramming/firstClass_Functions/ReturningFunctions.java
@@ -1,6 +1,17 @@
 package functionalProgramming.firstClass_Functions;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 public class ReturningFunctions {
+    protected static class MyMath {
+//        returns a function that takes an integer as an argument
+//        multiplies that argument by the integer that we passed it to the function
+        public static Function<Integer, Integer> createMultiplier(Integer y){
+            return (Integer x) -> x* y;
+
+        }
+    }
 
 
 
@@ -13,5 +24,10 @@ public class ReturningFunctions {
 
         NoArgFunction<String> greeter = creategreeter.apply();
         System.out.println(greeter.apply());
+
+//        defines how many times we want to multiple a number for
+        Function<Integer, Integer> timesTwo = MyMath.createMultiplier(2);
+//        passed the number that we are going to multiple
+        System.out.println(timesTwo.apply(11));
     }
 }
