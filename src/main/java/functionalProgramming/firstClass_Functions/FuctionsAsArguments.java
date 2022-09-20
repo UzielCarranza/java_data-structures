169d800413c75b4d78585e40f7275e2394e5b0b8
UzielCarranza <uzielcarranzac97@gmail.com>
2022-09-20 14:47:13 -0400

    creating a class that contains 2 functions, one that add number together and another one that subtracts numbers

---
+++ src/main/java/functionalProgramming/firstClass_Functions/FuctionsAsArguments.java
@@ -0,0 +1,22 @@
+package functionalProgramming.firstClass_Functions;
+
+import java.util.function.BiFunction;
+
+public class FuctionsAsArguments {
+
+    protected static class MyMath {
+        public static Integer add(Integer x, Integer y) {
+            return x + y;
+
+        }
+
+        public static Integer subtract(Integer x, Integer y) {
+            return x - y;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
