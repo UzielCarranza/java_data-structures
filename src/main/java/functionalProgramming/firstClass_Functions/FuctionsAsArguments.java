9f29cf659be2175be32a26fb0f887c65e5bb9806
UzielCarranza <uzielcarranzac97@gmail.com>
2022-09-20 14:54:47 -0400

    calling a function that calls other functions as arguments

--- src/main/java/functionalProgramming/firstClass_Functions/FuctionsAsArguments.java
+++ src/main/java/functionalProgramming/firstClass_Functions/FuctionsAsArguments.java
@@ -14,9 +14,21 @@ public class FuctionsAsArguments {
             return x - y;
         }
 
+        //        function that receives a function that says what to do with two numbers that were already in a function
+        public static Integer combine2And3(BiFunction<Integer, Integer, Integer> combineFunction) {
+            return combineFunction.apply(2, 3);
+        }
     }
 
     public static void main(String[] args) {
+//        call predefined methods
+//        calls the combine 2 and 3 function that calls the add function
+        System.out.println(MyMath.combine2And3(MyMath::add));
+
+
+//        calls the combine 2 and 3 function that calls the subtract function
+        System.out.println(MyMath.combine2And3(MyMath::subtract));
+
 
     }
 }
