a48f7f3bf7b4ad06f175c8198acbb4f3e4a4d5a5
UzielCarranza <uzielcarranzac97@gmail.com>
2022-09-20 14:17:45 -0400

    incorporated custumized interfaces using lambda functions

--- src/main/java/functionalProgramming/firstClass_Functions/biFunctions.java
+++ src/main/java/functionalProgramming/firstClass_Functions/biFunctions.java
@@ -11,6 +11,16 @@ public class biFunctions {
 //        in this example, the first two parameters are the parameters that the function will take, and the
 //        3rd will be the type of rerutning data
         BiFunction<Integer, Integer, Integer> add = (x, y) -> x + y;
-        System.out.println(add.apply(5,5));
+        System.out.println(add.apply(5, 5));
+
+
+//        TRI FUNCTION
+//        uses the customized interface to create a function that takes three parameters
+        TriFunction<Integer, Integer, Integer, Integer> triFunction = (a, b, c) -> a + b + c;
+        System.out.println(triFunction.apply(2,2,2));
+
+//        NO ARGUMENTS FUNCTION
+        NoArgFunction<String> sayHello = () -> "Hello World";
+        System.out.println(sayHello.apply());
     }
 }
