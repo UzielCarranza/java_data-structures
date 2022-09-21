4e8bcc0ce42da57d770d4c07152073dd5b024e4e
UzielCarranza <uzielcarranzac97@gmail.com>
2022-09-21 14:56:50 -0400

    initial data to practice with streams and filter

---
+++ src/main/java/streams/StreamFilter.java
@@ -0,0 +1,16 @@
+package streams;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamFilter {
+
+    public static void main(String[] args) {
+        //        Initialize array of numbers
+        Integer[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+//        convert array to list
+        List<Integer> listOfIntegers = new ArrayList<>(Arrays.asList(intArray));
+    }
+}
