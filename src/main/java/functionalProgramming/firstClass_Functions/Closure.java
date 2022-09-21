78d7ca2195ce57ecae2b1f2f70078c9bb71ea3eb
UzielCarranza <uzielcarranzac97@gmail.com>
2022-09-21 14:13:49 -0400

    created example of closure... function to be returned still has access scope to the fields inside of the returning funciton

--- src/main/java/functionalProgramming/firstClass_Functions/Closure.java
+++ src/main/java/functionalProgramming/firstClass_Functions/Closure.java
@@ -8,9 +8,16 @@ public class Closure {
 //        no arg function takes no parameters and returns a function that returns a string
         NoArgFunction<NoArgFunction<String>> createGreeter = () -> {
             String name = "Uziel";
-//            return a function
-            return () -> "Hello" + name;
+//            return a function that returns a string
+            return () -> "Hello" + " " + name;
         };
+
+//        create a function that takes no arguments and creates an instance of createGreeter
+        NoArgFunction<String> greeter = createGreeter.apply();
+
+//        OUTPUT EXAMPLE IS CLOSURE
+//        When we return a function, it still has access to the scope that it was returned from
+        System.out.println(greeter.apply());
 
     }
 }
