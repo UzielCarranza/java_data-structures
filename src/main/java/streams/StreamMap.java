385fc5d9f522263599f3a76efacea1b7425c541c
UzielCarranza <uzielcarranzac97@gmail.com>
2022-09-21 14:44:27 -0400

    added function that takes an argument and multiplies it by 2

--- src/main/java/streams/StreamMap.java
+++ src/main/java/streams/StreamMap.java
@@ -3,6 +3,7 @@ package streams;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 public class StreamMap {
 
@@ -11,7 +12,13 @@ public class StreamMap {
 //        Initialize array of numbers
         Integer[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+//        convert array to list
         List<Integer> listOfIntegers = new ArrayList<>(Arrays.asList(intArray));
         System.out.println(listOfIntegers);
+
+//        function that multiplies an argument by 2
+        Function<Integer, Integer> timesTwo = (x) -> x * 2;
+
+
     }
 }
