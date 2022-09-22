4cc338f90bef8a156f432fe911fde94d761ef823
UzielCarranza <uzielcarranzac97@gmail.com>
2022-09-22 09:45:50 -0400

    starting example for working with reduce

---
+++ src/main/java/streams/StreamReduce.java
@@ -0,0 +1,20 @@
+package streams;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamReduce {
+
+    public static void main(String[] args) {
+
+        //        Initialize array of numbers
+        Integer[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+//        convert array to list
+        List<Integer> listOfIntegers = new ArrayList<>(Arrays.asList(intArray));
+
+//        create binary unction that will be pass down to the reduce function
+
+    }
+}
