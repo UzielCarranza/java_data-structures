3e51f3568c5349130873cac82a848d97f7f596ad
UzielCarranza <uzielcarranzac97@gmail.com>
2022-09-22 10:15:08 -0400

    created an employee class to use as extra practice with stream

---
+++ src/main/java/streams/ExtraPractice.java
@@ -0,0 +1,22 @@
+package streams;
+
+
+public class ExtraPractice {
+
+//    create an Employee class
+    protected static class Employee{
+        public final String name;
+        public final Integer age;
+        public final String jobTitle;
+        public final Float salary;
+
+//        constructor
+
+    public Employee(String name, Integer age, String jobTitle, Float salary) {
+        this.name = name;
+        this.age = age;
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+    }
+}
+}
