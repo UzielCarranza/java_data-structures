63cc6697a3751beaec794dcf403c791c51d96429
UzielCarranza <uzielcarranzac97@gmail.com>
2022-09-22 09:52:49 -0400

    created function that will work eith reduce function to get the sum of numbers

--- src/main/java/streams/StreamReduce.java
+++ src/main/java/streams/StreamReduce.java
@@ -3,6 +3,7 @@ package streams;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BinaryOperator;
 
 public class StreamReduce {
 
@@ -15,6 +16,15 @@ public class StreamReduce {
         List<Integer> listOfIntegers = new ArrayList<>(Arrays.asList(intArray));
 
 //        create binary unction that will be pass down to the reduce function
+        BinaryOperator<Integer> getSum = (accumulator, currentElement) -> {
+//            get the sum from the accumulator and current element
+            Integer result = accumulator + currentElement;
+//            shows what happens when it's used on the reduce function
+            System.out.println("ACC: " + accumulator + " CURRENT ELEMENT " + currentElement + " RESULT: " + result);
+            return result;
+
+        };
+
 
     }
 }
