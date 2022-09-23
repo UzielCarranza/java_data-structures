2d6c250397663c183361264a57c67330efa60599
UzielCarranza <uzielcarranzac97@gmail.com>
2022-09-23 14:06:10 -0400

    created array of nums... converted array of nums to a type list

--- src/main/java/practiceFolder/IsEven.java
+++ src/main/java/practiceFolder/IsEven.java
@@ -1,4 +1,22 @@
 package practiceFolder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class IsEven {
+
+    public static void main(String[] args) {
+
+//        Create array of integers
+        Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+//        convert array of nums to a list of nums
+        List<Integer> numsToList = new ArrayList<Integer>(Arrays.asList(numbers));
+
+
+    }
 }
