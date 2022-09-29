a169381206bab73797e7848c6de2475529a47e00
UzielCarranza <uzielcarranzac97@gmail.com>
2022-09-29 19:23:38 -0400

    creating class to create a password complexity evaluator

---
+++ src/main/java/practiceFolder/PasswordComplexityEvaluator.java
@@ -0,0 +1,11 @@
+package practiceFolder;
+
+public class PasswordComplexityEvaluator {
+//    Create a function that checks if a password has
+
+//    at least six characters
+//    at least one uppercase
+//   at least  one lower case
+//    at least one number
+
+}
