5f9f69cc30354dcb30f97c2d0c1c794e1a670caf
UzielCarranza <uzielcarranzac97@gmail.com>
2022-09-29 19:29:49 -0400

    creating starting password complexity function using the Function interface and lambda

--- src/main/java/practiceFolder/PasswordComplexityEvaluator.java
+++ src/main/java/practiceFolder/PasswordComplexityEvaluator.java
@@ -1,11 +1,18 @@
 package practiceFolder;
 
+import java.util.function.Function;
+
 public class PasswordComplexityEvaluator {
 //    Create a function that checks if a password has
 
-//    at least six characters
+    //    at least six characters
 //    at least one uppercase
 //   at least  one lower case
 //    at least one number
+    public static void main(String[] args) {
+        Function<String, Boolean> isPasswordComplex = (password) -> {
+            return false;
+        };
+    }
 
 }
