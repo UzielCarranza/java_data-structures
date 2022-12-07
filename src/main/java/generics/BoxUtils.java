8176bf20ab5f0da62013e085d7f6532979b80435
UzielCarranza <uzielcarranzac97@gmail.com>
2022-12-07 10:39:21 -0600

    implements a generic method as a utility outside of the generic class

---
+++ src/main/java/generics/BoxUtils.java
@@ -0,0 +1,11 @@
+package generics;
+
+public class BoxUtils {
+//    Outside of generic class
+
+//    note... T is not the return type. It is of int type
+    public static  <T> int getCountOfItems(Box<T> box){
+        return box.getItems().size();
+
+    }
+}
