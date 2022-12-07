ee383cf59e06f7fb6ca8cef9a88a573a85baa1a2
UzielCarranza <uzielcarranzac97@gmail.com>
2022-12-07 10:33:37 -0600

    tests generic methods

--- src/main/java/generics/TestBoxGenerics.java
+++ src/main/java/generics/TestBoxGenerics.java
@@ -10,5 +10,13 @@ public class TestBoxGenerics {
 
         Box<Fruit> fruitBox = new Box<>();
         System.out.println(fruitBox);
+
+//        testing generic methods
+        bookBox.addItem(new Book("Harry Potter", "JK Pauling", 0.5));
+        bookBox.addItem(new Book("Deep Work", "Cal Newport", 0.25));
+        System.out.println(bookBox.getLatestItem());
+        fruitBox.addItem(new Fruit("Mango","India","Yellow",0.5));
+        fruitBox.addItem(new Fruit("banana","Brazil","Green",0.3));
+        System.out.println(fruitBox.getLatestItem());
     }
 }
